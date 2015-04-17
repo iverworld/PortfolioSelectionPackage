2ab63a61b3b20053eac9b89edd3c8eeb50a06a88
iverworld <iverworld@yahoo.com.hk>
2015-04-17 23:56:50 +0800

    Updated get_instrument_list.

--- R/util.R
+++ R/util.R
@@ -6,7 +6,20 @@ get_required_field_names <- function(){
 }
 
 #' Get roll back date.
-#' @return A day back from today
+#' @return A day back from today.
 get_roll_date <- function(days){
     return (Sys.Date() - days);
+}
+
+#' Get instrument list.
+#' @return Instrument list.
+get_instrument_list <- function(group="all"){
+    t=read.csv("./config/instrument.cfg", sep=",", header=TRUE);
+    if(group=="all")
+    {
+        return (t[,2]);    
+    }else{
+        return (t[which(t[,1]==group),2]);
+    }
+    
 }
