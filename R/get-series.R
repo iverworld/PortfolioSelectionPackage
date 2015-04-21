81776cd59b6ca9e6b9ecdb643d10b5ab97f14291
iverworld <iverworld@yahoo.com.hk>
2015-04-22 07:49:43 +0800

    Added get_series_list.

--- R/get-series.R
+++ R/get-series.R
@@ -3,7 +3,7 @@
 #'@param series_name String.
 #'@param from_date Start day.
 #'@param is_rdiff Boolean, apply Quandl percentage difference transformation.
-#'@return A time series
+#'@return A time series.
 get_unmanaged_series <- function(series_name, from_date, is_rdiff=FALSE){
     if (is_rdiff)
     {
@@ -24,9 +24,46 @@ get_unmanaged_series <- function(series_name, from_date, is_rdiff=FALSE){
 #'@param series_name String.
 #'@param from_date Start day.
 #'@param is_rdiff Boolean, apply Quandl percentage difference transformation.
-#'@return A time series
+#'@return A time series.
 get_series <- function(series_name, from_date, is_rdiff=FALSE){
     quandl_data = get_unmanaged_series(series_name, from_date, is_rdiff);
     header_quandl_data = colnames(quandl_data) %in% get_required_field_names();
     return (quandl_data[, header_quandl_data]);
+}
+
+#' Get series list
+#' 
+#' @param group The name of group.
+#' @param rolling_days Number of days rolled back from today.
+#' @param is_rdiff Boolean, apply Quandl percentage difference transformation.
+#' @return List of time series.
+
+get_series_list <- function(group = "all", rolling_days, is_rdiff=FALSE){
+    instruments = get_instrument_list(group);
+    if (length(instruments) == 0)
+    {
+        return (zoo());    
+    }
+    else if (length(instruments) == 1)
+    {
+        return (get_series(instruments[1], 
+                           get_roll_date(rolling_days), 
+                           is_rdiff));
+    }
+    else
+    {
+        ret = get_series(instruments[1], 
+                       get_roll_date(rolling_days), 
+                       is_rdiff);
+        column_names = instruments;
+        instruments = instruments[-1];
+        for (instrument in instruments){
+            t = get_series(instrument, 
+                           get_roll_date(rolling_days),
+                           is_rdiff);
+            ret = merge(ret, t, all = TRUE);
+        }
+        colnames(ret) = column_names;
+        return (ret);
+    }
 }
